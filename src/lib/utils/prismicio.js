7042a7dc9b3ccd7b4bf0c53a1d897b23cf043d5d
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-12-19 23:39:02 +0100

    fetch information from prismic

---
+++ src/lib/utils/prismicio.js
@@ -0,0 +1,18 @@
+import * as prismic from '@prismicio/client'
+const repositoryName = 'lose-your-head-the-cliente-case'
+
+
+const createClient = ({request, fetch}) => {
+    const clientOptions = {
+        fetch
+    }
+    const client = prismic.createClient(repositoryName, clientOptions)
+    
+    if (request) {
+        client.enableAutoPreviewsFromReq(request);
+      }
+
+    return client
+}
+
+export default createClient
